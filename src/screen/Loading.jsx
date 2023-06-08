025f77ede0af700df33addcb34cf146732230a20
96seba <sebastian200047@gmail.com>
2023-06-07 23:02:38 -0400

    first commit ever

---
+++ src/screen/Loading.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import './Loading.css'
+
+
+export default function Loading() {
+
+
+    return (
+        <div className="loading-div">
+            <h2 className="loading-name">Sebastian </h2>
+            <h2 className="loading-lastname"> Ramirez</h2>
+        </div>
+    )
+}
